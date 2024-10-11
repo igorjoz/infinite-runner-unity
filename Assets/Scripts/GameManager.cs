d3c729a164128bdd54e647bc0a19bb6241b69c3a
Igor Jozefowicz <igorjozefowicz@gmail.com>
2024-10-12 01:03:47 +0200

    add Score & TextMeshPro

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.UI;
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     //Tworzymy statyczn¹ zmienn¹ przechowuj¹c¹ jedyny obiekt klasy GameManager (wg. wzorca Singletonu)
-    //Pozwoli to na odwo³anie siê do GameManagera w dowolnym miejscu projektu poprzez wywo³anie GameManager.instance
+    //Pozwoli to na odwo³¹nie siê do GameManagera w dowolnym miejscu projektu poprzez GameManager.instance
     public static GameManager instance;
+    //Pole na element UI Text s³u¿¹cy do wyœwietlania wyniku
+    public TextMeshProUGUI scoreText;
     //Pole zawieraj¹ce prêdkoœæ œwiata do którego bêdzie móg³ odwo³aæ siê dowolny obiekt w grze
-    //Domyœlnie ustawiamy 0.2f, ale wartoœæ mo¿na dopasowaæ w edytorze
-    public float worldScrollingSpeed = 0.2f;
+    //Ustawienie wartoœci nastêpuje w edytorze
+    public float worldScrollingSpeed;
+    //Pole na wynik
+    private float score;
     // Use this for initialization
     void Start()
     {
         //Podczas uruchomienia przypisujemy aktualn¹ instancjê do statycznego pola instance
         //!!! Nale¿y uwa¿aæ, ¿eby zawsze na scenie by³ dok³adnie jeden GameManager !!!
-        if (instance == null) 
-        {
-            instance = this;
-        }
+        instance = this;
+    }
+    void FixedUpdate()
+    {
+        //Co tick silnika fizyki dopisujemy do wyniku przebyt¹ odleg³oœæ i wywo³ujemy metodê wyœwietlaj¹c¹ wynik na ekranie
+        score += worldScrollingSpeed;
+        UpdateOnScreenScore();
+    }
+    void UpdateOnScreenScore()
+    {
+        //Wyœwietlamy na elemencie nasz wynik bez czêœci dziesiêtnej
+        scoreText.text = score.ToString("0");
     }
 }
