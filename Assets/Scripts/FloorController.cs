c0dadce0deaa8714497301bfb015f114ee0eab40
Igor Jozefowicz <igorjozefowicz@gmail.com>
2024-10-19 09:42:45 +0200

    add obstacles & tiles random choice: lesson7

--- Assets/Scripts/FloorController.cs
+++ Assets/Scripts/FloorController.cs
@@ -6,6 +6,7 @@ public class FloorController : MonoBehaviour
     //Pola do których przypisujemy du¿e kafelki z pod³og¹
     //Zak³adamy, ¿e na pocz¹tku do floorTile1 jest przypisany lewy kafelek
     public GameObject floorTile1, floorTile2;
+    public GameObject[] tiles;
 
     // UWAGA - korzystamy z metody FixedUpdate, która wywo³uje siê raz na tick silnika fizyki a nie co klatkê.
     // Pozwoli nam to uniezale¿niæ ruchy obiektów od iloœci wyœwietlanych w danym momencie klatek na sekundê
@@ -13,10 +14,8 @@ public class FloorController : MonoBehaviour
     {
         //Co tick silnika fizyki przesuwamy oba kafelki o worldScrolingSpeed
         // -= poniewa¿ chcemy przesuwaæ œwiat w lewo, czyli zmniejszaæ wspó³rzêdn¹ X
-        floorTile1.transform.position -= new
-        Vector3(GameManager.instance.worldScrollingSpeed, 0f, 0f);
-        floorTile2.transform.position -= new
-        Vector3(GameManager.instance.worldScrollingSpeed, 0f, 0f);
+        floorTile1.transform.position -= new Vector3(GameManager.instance.worldScrollingSpeed, 0f, 0f);
+        floorTile2.transform.position -= new Vector3(GameManager.instance.worldScrollingSpeed, 0f, 0f);
 
         //Jeœli œrodek prawego kafelka przejecha³ przez œrodek ekranu (x < 0) lewy kafelek przenosimy
 
@@ -25,11 +24,14 @@ public class FloorController : MonoBehaviour
         {
             //przesuwam lewy kafelek w prawo o 32 jednostki (czyli 2 jego szerokoœci)
 
-            floorTile1.transform.position += new Vector3(32f, 0f, 0f);
+            // CHANGE: floorTile1.transform.position += new Vector3(32f, 0f, 0f);
+            int tileIndex = Random.Range(0, tiles.Length);
+            var newTile = Instantiate(tiles[tileIndex], floorTile2.transform.position + new Vector3(16f, 0f, 0f), Quaternion.identity);
+
             //Zamieniam zmienne
-            var tmp = floorTile1;
+            // CHANGE: var tmp = floorTile1;
             floorTile1 = floorTile2;
-            floorTile2 = tmp;
+            floorTile2 = newTile;
         }
     }
 }
