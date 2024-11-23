b03b66199ce4305f3afba5dd84818e7f29401560
Igor Józefowicz <igorjozefowicz@gmail.com>
2024-11-23 22:58:36 +0100

    add lesson12 progress: MainMenu scene

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public TextMeshProUGUI highScoreValue;
+    public TextMeshProUGUI coinsValue;
+    public TextMeshProUGUI soundButtonText;
+
+    int highScore = 0;
+    int coins = 0;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("HighScoreValue"))
+        {
+            highScore = PlayerPrefs.GetInt("HighScoreValue");
+        }
+
+        if (PlayerPrefs.HasKey("Coins"))
+        {
+            coins = PlayerPrefs.GetInt("Coins");
+        }
+
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        highScoreValue.text = highScore.ToString();
+        coinsValue.text = coins.ToString();
+
+        if (SoundManager.instance.GetMuted())
+        {
+            soundButtonText.text = "TURN SOUND ON";
+        }
+        else
+        {
+            soundButtonText.text = "TURN SOUND OFF";
+        }
+    }
+
+    public void PlayButtonClicked()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void SoundButtonClicked()
+    {
+        SoundManager.instance.ToggleMuted();
+
+        UpdateUI();
+    }
+}
